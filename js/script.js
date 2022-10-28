fea4bb4e7a89d221291135432b4cd9999a99445a
giufeli <giuliofelicetti99@gmail.com>
2022-10-28 16:44:14 +0200

    Commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 const bottoneVerify = document.getElementById("button")
-
+parolaInserita = document.getElementById("testo").value;
 bottoneVerify.addEventListener('click', function (){
-    parolaInserita = document.getElementById("testo").value;
-
+    
     if (isPali(parolaInserita)){
-        alert( 'E\' una parola palindrome');
-        console.log( 'E\' una parola palindrome');
+        alert( 'Questa parola è palindrome!');
+        console.log( 'Questa parola è palindrome!');
     } else {
-        alert ('Non è palindrome')
-        console.log('Non è palindrome'); 
-    }
+        alert ('Questa parola non è palindrome')
+        console.log('Questa parola non è palindrome'); 
+    };
     
     function isPali(testo) {
         if(Array.from(testo).toString() == (Array.from(testo).reverse()).toString()) {
@@ -20,6 +19,27 @@ bottoneVerify.addEventListener('click', function (){
     }
 
 });
+
+
+const bottoneNumero = document.getElementById("buttonNumero")
+
+
+bottoneNumero.addEventListener('click', function (){
+    numeroInserito = document.getElementById("numero").value;
+    paroDisparo = document.getElementById("genere").value;
+    numeroComputer = (Math.floor(Math.random() * 5) + 1);
+
+    console.log(numeroInserito);
+    console.log(numeroComputer)
+    
+    
+    function somma(num1, num2){
+        return(num1 + num2);
+    }
+
+    console.log(somma(numeroInserito + numeroComputer)); 
+    
+})
 
 
 
